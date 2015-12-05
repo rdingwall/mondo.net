f0d8cf4603c49b0887c5413e876892f741edcb9b
Richard Dingwall <rdingwall@gmail.com>
2015-12-05 16:11:04 +0000

    Naming

--- Mondo.Sample/Program.cs
+++ Mondo.Sample/Program.cs
@@ -19,17 +19,17 @@ namespace Mondo.Sample
                 Console.Write("Password: ");
                 string password = Console.ReadLine();
 
-                using (var mondoClient = new MondoClient(url, clientId, clientSecret))
+                using (var client = new MondoClient(url, clientId, clientSecret))
                 {
-                    mondoClient.RequestAccessTokenAsync(username, password).Wait();
+                    client.RequestAccessTokenAsync(username, password).Wait();
 
-                    IList<Account> accounts = mondoClient.ListAccountsAsync().Result;
+                    IList<Account> accounts = client.ListAccountsAsync().Result;
                     foreach (Account account in accounts)
                     {
                         Console.WriteLine("Account: id={0}, created={1}, description={2}", account.Id, account.Created, account.Description);
                     }
 
-                    IList<Transaction> transactions = mondoClient.ListTransactionsAsync(accounts[0].Id).Result;
+                    IList<Transaction> transactions = client.ListTransactionsAsync(accounts[0].Id).Result;
                     foreach (Transaction transaction in transactions)
                     {
                         Console.WriteLine("Transaction: created={0}, description={1}, amount={2}, currency={3}, accountBalance={4}", transaction.Created, transaction.Description, transaction.Amount, transaction.Currency, transaction.AccountBalance);
